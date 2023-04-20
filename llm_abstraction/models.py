051941cab66f57a0f76c051f2e06dd39981e07ff
Andrew Green <agreen@ebi.ac.uk>
2023-04-20 13:56:41 +0100

    Move temperature kwarg to the kwars provided at instantiation

--- llm_abstraction/models.py
+++ llm_abstraction/models.py
@@ -6,9 +6,7 @@ from langchain.chat_models import ChatOpenAI
 def get_model(source: str, kwargs):
     if source == "chatGPT":
         logging.info("Initializing OpenAI chatGPT LLM")
-        llm = ChatOpenAI(
-            model_name="gpt-3.5-turbo", temperature=0.1, model_kwargs=kwargs
-        )
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", model_kwargs=kwargs)
     elif source == "llama-7B":
         logging.info("Initializing Facebook LLaMA 7B model")
         logging.warn("LLaMA is not yet implemented!")
