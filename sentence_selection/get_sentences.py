2a036d5c369475f444e7f85ee67b0f03a8074784
Andrew Green <agreen@ebi.ac.uk>
2023-05-23 15:12:10 +0100

    Refactor sentence selection code

---
+++ sentence_selection/get_sentences.py
@@ -0,0 +1,60 @@
+import os
+import typing as ty
+from pathlib import Path
+
+import polars as pl
+from pull_sentences import pull_data_from_db
+from sentence_selector import iterative_sentence_selector
+
+from utils import get_token_length
+
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+
+from sentence_transformers import SentenceTransformer
+
+
+def sample_sentences(
+    sentences: pl.DataFrame, model: SentenceTransformer, limit: ty.Optional[int] = 3072
+):
+    df = sentences.with_columns(
+        pl.struct(["sentence", "job_id"])
+        .apply(lambda x: iterative_sentence_selector(x, model, limit))
+        .alias("result")
+    ).unnest("result")
+    return df
+
+
+def tokenize_and_count(sentence_df: pl.DataFrame, limit: ty.Optional[int] = 3072):
+    df = sentence_df.with_columns(
+        [pl.col("sentence").apply(get_token_length).alias("num_tokens")]
+    )
+    df = df.with_columns(
+        [
+            pl.col("num_tokens").arr.sum().alias("total"),
+            pl.col("pmcid").arr.lengths().alias("num_articles"),
+        ]
+    ).sort("total", descending=True)
+    print(
+        f"Number of entities with fewer than {limit} total tokens: {df.filter(pl.col('total').lt(limit)).height} before selection"
+    )
+    return df
+
+
+def for_summary(
+    conn_str: str,
+    query: str,
+    cache: Path,
+    device: ty.Optional[str] = "cpu:0",
+    limit: ty.Optional[int] = 3072,
+) -> pl.DataFrame:
+    if Path(cache).exists():
+        sentence_df = pl.read_json(cache)
+    else:
+        sentence_df = pull_data_from_db(conn_str, query)
+        sentence_df = tokenize_and_count(sentence_df)
+        sentence_df.write_json(cache)
+
+    model = SentenceTransformer("all-MiniLM-L6-v2", device=device)
+    sample_df = sample_sentences(sentence_df, model, limit)
+
+    return sample_df.select(["job_id", "selected_pmcids", "selected_sentences"])
