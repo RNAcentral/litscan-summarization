f638265fcef851ea601e97f495ea5226b8e15e59
Andrew Green <agreen@ebi.ac.uk>
2023-05-25 14:13:15 +0100

    Move prefiltering into utils

--- sentence_selection/utils.py
+++ sentence_selection/utils.py
@@ -1,3 +1,4 @@
+import polars as pl
 import tiktoken
 
 enc = tiktoken.get_encoding("cl100k_base")
@@ -5,3 +6,28 @@ enc = tiktoken.get_encoding("cl100k_base")
 
 def get_token_length(sentences):
     return [len(enc.encode(s)) for s in sentences]
+
+
+def prefilter_sentences(df, regexes=None):
+    """
+    Use a regex to remove sentences that are found in tables, figures or supplementary material
+
+    Optionally apply some user supplied regexes to remove sentences that match those regexes.
+    """
+    filtered_df = df.lazy().filter(
+        pl.col("sentence")
+        .arr.eval(
+            ~pl.element().str.contains("image|figure|table|supplementary material")
+        )
+        .collect()
+    )
+
+    if regexes:
+        for regex in regexes:
+            filtered_df = (
+                filtered_df.lazy()
+                .filter(pl.col("sentence").str.contains(regex).is_not())
+                .collect()
+            )
+
+    return filtered_df
