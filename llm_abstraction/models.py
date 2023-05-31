3ca7c7ed3a43fdb7a016db89708e367a817ec6f9
Andrew Green <agreen@ebi.ac.uk>
2023-05-31 17:11:58 +0100

    langchain now requires temp to be explicit,

--- llm_abstraction/models.py
+++ llm_abstraction/models.py
@@ -4,9 +4,15 @@ from langchain.chat_models import ChatOpenAI
 
 
 def get_model(source: str, kwargs):
+    assert "temperature" in kwargs, "temperature must be specified"
+    ## Langchain wants temp explicitly stated, so here we go
+    temperature = kwargs["temperature"]
+    del kwargs["temperature"]
     if source == "chatGPT":
         logging.info("Initializing OpenAI chatGPT LLM")
-        llm = ChatOpenAI(model_name="gpt-3.5-turbo", model_kwargs=kwargs)
+        llm = ChatOpenAI(
+            model_name="gpt-3.5-turbo", temperature=temperature, model_kwargs=kwargs
+        )
     elif source == "llama-7B":
         logging.info("Initializing Facebook LLaMA 7B model")
         logging.warn("LLaMA is not yet implemented!")
