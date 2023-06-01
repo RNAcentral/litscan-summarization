8f5588ad23aecffa1acdba6fa5e23ed229aede14
Andrew Green <agreen@ebi.ac.uk>
2023-06-01 15:47:54 +0100

    Reuse LitScan function to get credentials from environment

--- summarize/utils/database.py
+++ summarize/utils/database.py
@@ -1,6 +1,20 @@
 import logging
+import os
+from collections import namedtuple
 
 import polars as pl
+
+Settings = namedtuple(
+    "Settings",
+    [
+        "POSTGRES_HOST",
+        "POSTGRES_PORT",
+        "POSTGRES_DATABASE",
+        "POSTGRES_USER",
+        "POSTGRES_PASSWORD",
+        "ENVIRONMENT",
+    ],
+)
 
 
 def insert_rna_data(data_dict, conn_str, interactive=False):
@@ -29,6 +43,47 @@ def insert_rna_data(data_dict, conn_str, interactive=False):
             df.write_database(
                 "litscan_article_summaries", conn_str, if_exists="replace"
             )
+
+
+def get_postgres_credentials(ENVIRONMENT):
+    ENVIRONMENT = ENVIRONMENT.upper()
+
+    if ENVIRONMENT == "DOCKER":
+        return Settings(
+            POSTGRES_HOST="database",  # database image from docker compose
+            POSTGRES_PORT=5432,
+            POSTGRES_DATABASE=os.getenv("LITSCAN_DB", "reference"),
+            POSTGRES_USER=os.getenv("LITSCAN_USER", "docker"),
+            POSTGRES_PASSWORD=os.getenv("LITSCAN_PASSWORD", "example"),
+            ENVIRONMENT=ENVIRONMENT,
+        )
+    elif ENVIRONMENT == "LOCAL":
+        return Settings(
+            POSTGRES_HOST="localhost",
+            POSTGRES_PORT=5432,
+            POSTGRES_DATABASE="reference",
+            POSTGRES_USER="docker",
+            POSTGRES_PASSWORD="example",
+            ENVIRONMENT=ENVIRONMENT,
+        )
+    elif ENVIRONMENT == "PRODUCTION":
+        return Settings(
+            POSTGRES_HOST=os.getenv("POSTGRES_HOST", "192.168.0.6"),
+            POSTGRES_PORT=os.getenv("POSTGRES_PORT", 5432),
+            POSTGRES_DATABASE=os.getenv("POSTGRES_DATABASE", "reference"),
+            POSTGRES_USER=os.getenv("POSTGRES_USER", "docker"),
+            POSTGRES_PASSWORD=os.getenv("POSTGRES_PASSWORD", "pass"),
+            ENVIRONMENT=ENVIRONMENT,
+        )
+    elif ENVIRONMENT == "TEST":
+        return Settings(
+            POSTGRES_HOST="localhost",
+            POSTGRES_PORT=5432,
+            POSTGRES_DATABASE="test_reference",
+            POSTGRES_USER="docker",
+            POSTGRES_PASSWORD="example",
+            ENVIRONMENT="LOCAL",
+        )
 
 
 if __name__ == "__main__":
