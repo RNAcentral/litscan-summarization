0df6cbc0105a25ddce4535a19fce20ab1a585bb8
Andrew Green <agreen@ebi.ac.uk>
2023-06-06 12:00:46 +0100

    connextorx is not working, fall back to psycopg

--- summarize/utils/database.py
+++ summarize/utils/database.py
@@ -3,6 +3,8 @@ import os
 from collections import namedtuple
 
 import polars as pl
+import psycopg2
+from psycopg2.extras import execute_values
 
 Settings = namedtuple(
     "Settings",
@@ -18,31 +20,60 @@ Settings = namedtuple(
 
 
 def insert_rna_data(data_dict, conn_str, interactive=False):
-    df = pl.DataFrame(data_dict)
-    try:
-        df.write_database("litscan_article_summaries", conn_str, if_exists="fail")
-    except:
-        logging.warning("Data already exists in database!")
-        if interactive:
+    conn = psycopg2.connect(conn_str)
+    cur = conn.cursor()
+    if interactive:
+        num = cur.execute("select count(*) from litsumm_summaries").fetchone()
+        if num > 0:
             choice = input(
-                "Data already exists in database! Would you like to (o)verwrite, (a)ppend, or (s)kip?"
+                f"Data already exists in database! Would you like to (o)verwrite, (a)ppend, or (s)kip? ({num} rows)"
             )
             if choice == "o":
-                df.write_database(
-                    "litscan_article_summaries", conn_str, if_exists="replace"
-                )
+                cur.execute("truncate table litsumm_summaries")
             elif choice == "a":
-                df.write_database(
-                    "litscan_article_summaries", conn_str, if_exists="append"
-                )
+                logging.warning("Appending data!")
             elif choice == "s":
                 logging.warning("Skipping!")
                 print("Skipping!")
-        else:
-            logging.warning("Overwriting data in database!")
-            df.write_database(
-                "litscan_article_summaries", conn_str, if_exists="replace"
-            )
+                return
+    else:
+        logging.warning("Overwriting data in database!")
+        cur.execute("truncate table litsumm_summaries")
+
+    data = [
+        (e["ent_id"], e["context"], e["summary"], e["cost"], e["total_tokens"])
+        for e in data_dict
+    ]
+    insert_query = "insert into litsumm_summaries (rna_id, context, summary, cost, total_tokens) values %s"
+    execute_values(cur, insert_query, data, page_size=100)
+    conn.commit()
+    cur.close()
+    conn.close()
+    # df = pl.DataFrame(data_dict)
+    # try:
+    #     df.write_database("litscan_article_summaries", conn_str, if_exists="fail")
+    # except:
+    #     logging.warning("Data already exists in database!")
+    #     if interactive:
+    #         choice = input(
+    #             "Data already exists in database! Would you like to (o)verwrite, (a)ppend, or (s)kip?"
+    #         )
+    #         if choice == "o":
+    #             df.write_database(
+    #                 "litscan_article_summaries", conn_str, if_exists="replace"
+    #             )
+    #         elif choice == "a":
+    #             df.write_database(
+    #                 "litscan_article_summaries", conn_str, if_exists="append"
+    #             )
+    #         elif choice == "s":
+    #             logging.warning("Skipping!")
+    #             print("Skipping!")
+    #     else:
+    #         logging.warning("Overwriting data in database!")
+    #         df.write_database(
+    #             "litscan_article_summaries", conn_str, if_exists="replace"
+    #         )
 
 
 def get_postgres_credentials(ENVIRONMENT):
