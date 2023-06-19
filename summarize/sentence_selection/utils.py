e50195a315f2949bc29d56b6c86b97fb782b8b94
Andrew Green <agreen@ebi.ac.uk>
2023-06-19 11:54:30 +0100

    Add a plotting utility for topic modelling

--- summarize/sentence_selection/utils.py
+++ summarize/sentence_selection/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import polars as pl
 import tiktoken
 
@@ -33,3 +34,42 @@ def prefilter_sentences(df, regexes=None):
             )
 
     return filtered_df
+
+
+def plot(clustered, outliers):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    ## Plot the clusters
+    fig = plt.figure(figsize=(10, 7))
+    sns.scatterplot(
+        x=outliers.select("x").to_numpy().flatten(),
+        y=outliers.select("y").to_numpy().flatten(),
+        color="grey",
+        alpha=0.4,
+    )
+    ax = sns.scatterplot(
+        x=clustered.select("x").to_numpy().flatten(),
+        y=clustered.select("y").to_numpy().flatten(),
+        hue=clustered.select("sentence_labels").to_numpy().flatten(),
+        legend="full",
+        alpha=0.7,
+        palette=sns.color_palette(
+            "hls",
+            len(np.unique(clustered.select("sentence_labels").to_numpy().flatten())),
+        ),
+    )
+    handles, labels = ax.get_legend_handles_labels()
+    print(clustered.get_column("topics").unique().to_numpy())
+    lgd = ax.legend(
+        handles,
+        clustered.get_column("topics").unique().to_list(),
+        bbox_to_anchor=(1, 1),
+        title=None,
+        frameon=False,
+    )
+    # sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1), title=None, frameon=False)
+
+    # fig.tight_layout()
+    # plt.savefig("sentence_topics_model1.png",bbox_extra_artists=(lgd,), bbox_inches='tight')
+    plt.show()
