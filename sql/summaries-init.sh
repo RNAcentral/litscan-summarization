b02c35b61db69cf79a236563f0a7c15244bf659d
Andrew Green <agreen@ebi.ac.uk>
2023-06-26 15:02:19 +0100

    Modify database initialisation for feedback inclusion

--- sql/summaries-init.sh
+++ sql/summaries-init.sh
@@ -11,11 +11,36 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     summary text,
     cost float,
     total_tokens integer,
+    problem_summary boolean,
     attempts integer,
     truthful boolean,
     consistency_check_result text,
     selection_method text
   );
   ALTER TABLE public.litsumm_summaries OWNER TO $LITSCAN_USER;
+  COMMIT;
+EOSQL
+
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  \connect $LITSCAN_DB $LITSCAN_USER
+  BEGIN;
+  CREATE TABLE litsumm_feedback_single (
+    feedback_id serial PRIMARY KEY,
+    user_id text,
+    summary_id int,
+    feedback int,
+    contains_hallucinations boolean,
+    inaccurate_text boolean,
+    contradictory boolean,
+    over_specific boolean,
+    bad_length boolean,
+    mentions_ai boolean,
+    short_context boolean,
+    false_positive boolean,
+    free_feedback text,
+    FOREIGN KEY (summary_id) REFERENCES litsumm_summaries(id)
+  );
+  ALTER TABLE public.litsumm_feedback_single OWNER TO $LITSCAN_USER;
   COMMIT;
 EOSQL
