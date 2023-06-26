8017fdc179dfd49b545b55e02082040b8a02617c
Andrew Green <agreen@ebi.ac.uk>
2023-06-26 10:24:22 +0100

    Remove parentised citations from the context.
    These look like hallucinations, so we want them gone

--- summarize/utils/context.py
+++ summarize/utils/context.py
@@ -7,7 +7,15 @@ def build_context(sentences, pmcids, joining_str=" "):
     """
     cited_sentences = []
     for sentence, pmcid in zip(sentences, pmcids):
+        ## Find and remove anything in square brackets
         sentence = re.sub(r"\[.+\]", "", sentence).strip().replace("\n", " ")
+        ## Find and remove anything that looks like a 'normal' citation
+        sentence = (
+            re.sub(r"\((?:[A-Za-z\s]+ et al\., \d{4}(?:; )?)+\)", "", sentence)
+            .strip()
+            .replace("\n", " ")
+        )
+
         cited_sentences.append(f"{sentence.strip('.')} [{pmcid}].")
 
     context = joining_str.join(cited_sentences)
