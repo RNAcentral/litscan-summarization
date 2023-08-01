Fix normalisation for eval loss stretching

reward_model/train_nn_classifier.py
@@ -184,7 +184,7 @@ def main(
         eval_accs.append(metric_eval.compute()["accuracy"])
 
     train_steps = np.arange(len(train_losses))
-    eval_steps = np.arange(len(eval_losses)) * num_training_steps
+    eval_steps = np.arange(len(eval_losses)) * len(train_losses) / len(eval_losses)
 
     plt.plot(train_steps, train_losses, label="Train")
     plt.plot(eval_steps, eval_losses, label="Eval")