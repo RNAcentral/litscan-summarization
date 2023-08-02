6737537ffea77e6b1f91f235d328eb08da45ab2b
Andrew Green <agreen@ebi.ac.uk>
2023-08-02 14:59:01 +0100

    Fix NN layer dimensions for summary only model

--- reward_model/train_nn_classifier.py
+++ reward_model/train_nn_classifier.py
@@ -82,7 +82,7 @@ class SummaryEvaluator(nn.Module):
         )
 
         self.dropout = nn.Dropout(0.25)
-        self.classifier = nn.Linear(2 * 768, num_classes)
+        self.classifier = nn.Linear(768, num_classes)
         self.num_classes = num_classes
 
     def forward(
