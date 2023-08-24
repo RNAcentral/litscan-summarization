817bd53f032d2032b16a34f389ec4075a4dd32f3
Andrew Green <agreen@ebi.ac.uk>
2023-08-24 14:25:20 +0100

    Tweaks for codon

--- automatic_evaluation/calculate_bert_score.py
+++ automatic_evaluation/calculate_bert_score.py
@@ -1,16 +1,14 @@
 import os
 
 import click
-import matplotlib.pyplot as plt
-import numpy as np
 import polars as pl
 from bert_score import score
-from scipy.stats import spearmanr
 
 model = "allenai/longformer-base-4096"
 
 
-def calculate_bert_score(contexts, summaries):
+def calculate_bert_score(row, device, batch_size):
+    row.get_column("context").to_numpy(), row.get_column("summary").to_numpy()
     contexts = [c.strip() for c in contexts]
     summaries = [s.strip() for s in summaries]
     P, R, F1 = score(
@@ -19,8 +17,8 @@ def calculate_bert_score(contexts, summaries):
         lang="en",
         verbose=True,
         model_type=model,
-        device="mps",
-        batch_size=1,
+        device=device,
+        batch_size=batch_size,
     )
     return {"precision": P, "recall": R, "f1": F1}
 
@@ -28,15 +26,18 @@ def calculate_bert_score(contexts, summaries):
 @click.command()
 @click.argument("input_file")
 @click.argument("output_file")
-def main(input_file, output_file):
+@click.option("--device", default="cpu")
+@click.option("--batch_size", default=16)
+def main(input_file, output_file, device, batch_size):
     data = pl.read_parquet(input_file)
     print(data)
-    bert_scores = calculate_bert_score(
-        data.get_column("context").to_numpy(), data.get_column("summary").to_numpy()
-    )
+    data = data.with_columns(
+        result=pl.struct(["context", "summary"]).apply(
+            lambda x: calculate_bert_score(x, device, batch_size)
+        )
+    ).unnest("result")
 
-    counts, bins, patches = plt.hist(bert_scores["f1"], bins=100, label="BERTScore")
-    plt.show()
+    data.write_parquet(output_file)
 
 
 if __name__ == "__main__":
